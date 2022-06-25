c8ddeac78de7b9f3452dc9535756d55d05d54f12
ruixue0702 <ruixue0702@163.com>
2022-06-25 15:58:31 +0800

    init

---
+++ .eslintrc.js
@@ -0,0 +1,20 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true
+  },
+  extends: [
+    // 'eslint:recommended',
+    // 'plugin:vue/essential', // vue2版本（默认配置）
+    'plugin:@typescript-eslint/recommended', // 放在plugin:vue/vue3-recommended前面，避免vue3的被覆盖
+    'plugin:vue/vue3-recommended', // vue3版本（识别vue3代码）
+    'plugin:prettier/recommended' // prettier 校验
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    parser: '@typescript-eslint/parser',
+    sourceType: 'module'
+  },
+  plugins: ['vue', '@typescript-eslint'],
+  rules: {}
+}
---
+++ .prettierrc.js
@@ -0,0 +1,20 @@
+module.exports = {
+  // printWidth: 80,
+  // tabWidth: 2,
+  // useTabs: false,
+  semi: false, // 末尾逗号 default: true
+  singleQuote: true, // 单引号 default: false
+  // quoteProps: 'as-needed',
+  // jsxSingleQuote: false,
+  trailingComma: "none", // 末尾分号 default: es5
+  // bracketSpacing: true,
+  // bracketSameLine: false,
+  // jsxBracketSameLine: false,
+  arrowParents: "avoid", // default: always
+  // insertPragma: false,
+  // requirePragma: false,
+  proseWrap: "never",
+  // htmlWhitespaceSensitivity: 'css',
+  // vueIndentScriptAndStyle: false, // .vue缩进
+  endOfLine: "auto", // default: lf
+}
